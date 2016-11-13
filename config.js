e4b16908cbecc15f8d079e36b7460cab60a6f5d9
Langlois <stephane.langlois@scopyleft.fr>
2016-11-13 12:54:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ var GH = {}
 GH.SECRET = 'YWU0ZmRkZGFjOTVlZGM1ZTc1MmI3NjRjZTI3Y2UxZGYyMzdmZTdkZg=='
 GH.ID = 'ODUzMjY1YjA5YjBjMjVlOTg2MTQ='
 // Github Owner's
-GH.OWNER = 'multibao'
+GH.OWNER = 'pleinphare'
 // Github crew's repository
 GH.CREW = 'organisations'
 
 var MULTIBAO = {}
-MULTIBAO.UVP1 = 'Partager et enrichir vos pratiques d\'équipe'
-MULTIBAO.UVP2 = 'Let\'s Do It Together'
-MULTIBAO.BUTTON1 = 'Découvrir'
-MULTIBAO.BUTTON2 = 'Espace contributeurs / bloggeurs'
+MULTIBAO.UVP1 = 'Mieux cerner qui je suis à travers des ateliers'
+MULTIBAO.UVP2 = 'pour engager une activité qui a du sens pour moi'
+MULTIBAO.BUTTON1 = ''
+MULTIBAO.BUTTON2 = ''
